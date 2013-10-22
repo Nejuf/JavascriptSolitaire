9f41d2ad2d80d2af5ad82b613ddae8c2b7d44b40
Isaac Murchie and Nejuf <imurchie@gmail.com>
2013-10-22 15:28:47 -0700

    Cleans code

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -2,12 +2,15 @@
 	var TicTacToe = root.TicTacToe = (root.TicTacToe || {});
 
 	var Game = TicTacToe.Game = function() {
-		this.board = new Board();
+		this.board = new TicTacToe.Board();
 		this.player = "x";
 	}
 
 	var readline = require('readline');
-	var reader = readline.createInterface({ input: process.stdin, output: process.stdout});
+	var reader = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
 
 	Game.prototype.promptForMove = function(player, callback) {
 		reader.question(player + ": what is your next move? ", function(answer) {
@@ -19,14 +22,14 @@
 		});
 	}
 
-	Game.prototype.moveCallback = function(player, x, y) {
-		if(this.board.validPos(x,y) && this.board.posEmpty(x,y)) {
+	Game.prototype.move = function(player, x, y) {
+		if(this.board.isValidPos(x,y) && this.board.isEmptyPos(x,y)) {
 			this.board.setPos(player, x, y);
 			this.nextPlayer();
 			this.checkWin();
 		} else {
 			console.log("Invalid move");
-			this.promptForMove(player, this.moveCallback.bind(this));
+			this.promptForMove(player, this.move.bind(this));
 		}
 	}
 
@@ -34,11 +37,11 @@
 
 		var winner = this.board.winner();
 
-		if(winner) {
+		if (winner) {
 			this.board.show();
 			console.log(winner + " wins!");
 			reader.close();
-		} else if(this.board.is_full()) {
+		} else if (this.board.isFull()) {
 			this.board.show();
 			console.log("Draw.");
 			reader.close();
@@ -49,7 +52,7 @@
 
 	Game.prototype.run = function() {
 		this.board.show();
-		this.promptForMove(this.player, this.moveCallback.bind(this));
+		this.promptForMove(this.player, this.move.bind(this));
 	}
 
 	Game.prototype.nextPlayer = function(){
@@ -61,9 +64,9 @@
 		this.grid = [ [1, 1, 1], [1, 1, 1], [1, 1, 1] ];
 	}
 
-	Board.prototype.is_full = function(){
-		for(var i=0; i<3; i++) {
-			for(var j=0; j<3; j++) {
+	Board.prototype.isFull = function(){
+		for(var i=0; i < 3; i++) {
+			for(var j=0; j < 3; j++) {
 				if(this.grid[i][j] === 1) return false;
 			}
 		}
@@ -78,11 +81,11 @@
 		this.lines.forEach(function(line) {
 			if(winner) return;
 
-			var piece = that.grid[line[0][0]][line[0][1]];
-			if(piece === 1) return;
+			if(that.isEmptyPos(line[0][0], line[0][1])) return;
 
-			if(piece === that.grid[line[1][0]][line[1][1]] &&
-				 piece === that.grid[line[2][0]][line[2][1]]) {
+			var piece = that.getPos(line[0][0],line[0][1]);
+			if(piece === that.getPos(line[1][0],line[1][1]) &&
+				 piece === that.getPos(line[2][0],line[2][1])) {
 					 winner = piece;
 				}
 		});
@@ -109,11 +112,11 @@
 			return this.grid[x][y];
 		}
 
-		Board.prototype.posEmpty = function(x,y){
+		Board.prototype.isEmptyPos = function(x,y){
 			return this.grid[x][y] === 1;
 		}
 
-		Board.prototype.validPos = function(x,y){
+		Board.prototype.isValidPos = function(x,y){
 			return ((x >= 0 && x < 3) && (y >= 0 && y < 3));
 		}
 
@@ -121,7 +124,7 @@
 			for(var i=0; i<3; i++) {
 				var row = "";
 				for(var j=0; j<3; j++) {
-					row += (this.posEmpty(i,j)) ? "-" : this.getPos(i,j);
+					row += (this.isEmptyPos(i,j)) ? "-" : this.getPos(i,j);
 				}
 				console.log(row);
 			}
